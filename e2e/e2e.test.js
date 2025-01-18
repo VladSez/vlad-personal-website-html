9992a96f81eb209b25ab87348715a1b6cf6c8479
Vlad Sazonau <16666234+VladSez@users.noreply.github.com>
2025-01-18 21:48:55 +0100

    chore: update .gitignore and enhance index.html metadata and content (#35)
    * chore: update .gitignore and enhance index.html metadata and content
    
    - Added additional entries to .gitignore for various debug and miscellaneous files.
    - Updated meta tags in index.html to improve SEO, including author and keywords.
    - Added a new section in index.html to showcase personal projects with links.
    
    * update open graph meta tags

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -18,7 +18,7 @@ test("/index.html", async ({ page }) => {
   const metaDescription = page.locator('meta[name="description"]');
   await expect(metaDescription).toHaveAttribute(
     "content",
-    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack enthusiast."
+    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack developer and enthusiast."
   );
 
   // open graph meta tags
@@ -98,7 +98,7 @@ test("/links.html", async ({ page }) => {
   const metaDescription = page.locator('meta[name="description"]');
   await expect(metaDescription).toHaveAttribute(
     "content",
-    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack enthusiast."
+    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack developer and enthusiast."
   );
 
   const ogTitle = page.locator('meta[property="og:title"]');
@@ -353,7 +353,7 @@ test("/videos.html", async ({ page }) => {
   const metaDescription = page.locator('meta[name="description"]');
   await expect(metaDescription).toHaveAttribute(
     "content",
-    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack enthusiast."
+    "Vlad Sazonau personal website and blog. Vlad Sazonau is a frontend/full-stack developer and enthusiast."
   );
 
   // open graph meta tags
